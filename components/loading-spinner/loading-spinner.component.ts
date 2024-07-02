6290962756e9bfe417edc306f74382fbe6711545
MoulinZ <zacharie.moulin@gmail.com>
2024-07-02 14:17:16 +0200

    affichage progress dans loading spinner

--- components/loading-spinner/loading-spinner.component.ts
+++ components/loading-spinner/loading-spinner.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-loading-spinner',
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./loading-spinner.component.scss']
 })
 export class LoadingSpinnerComponent {
+
+  @Input()
+  progress : number | undefined;
 
 }
