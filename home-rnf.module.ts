6ff9e4d8ccadf39c62609473831c45612e980b05
MoulinZ <zacharie.moulin@gmail.com>
2024-10-30 09:00:46 +0100

    ajout de la bibliothèque regular de fontawesome

--- home-rnf.module.ts
+++ home-rnf.module.ts
@@ -1,30 +1,31 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AppRoutingModule } from '../app-routing.module';
+import { AccessDeniedComponent } from './components/access-denied/access-denied.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { LoadingSpinnerComponent } from './components/loading-spinner/loading-spinner.component';
 import { LoginComponent } from './components/login/login.component';
 import { LogoutComponent } from './components/logout/logout.component';
 import { NavHomeComponent } from './components/nav-home/nav-home.component';
-import { InputErrorPipe } from './pipes/input-error.pipe';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from '../app-routing.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { HttpClientModule } from '@angular/common/http';
-import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatInputModule } from '@angular/material/input';
-import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { fab } from '@fortawesome/free-brands-svg-icons';
-import { fas } from '@fortawesome/free-solid-svg-icons';
-import { LoadingSpinnerComponent } from './components/loading-spinner/loading-spinner.component';
-import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
-import { AccessDeniedComponent } from './components/access-denied/access-denied.component';
+import { InputErrorPipe } from './pipes/input-error.pipe';
 
 @NgModule({
   declarations: [
@@ -49,7 +50,7 @@ import { AccessDeniedComponent } from './components/access-denied/access-denied.
     MatDialogModule,
     MatProgressBarModule,
     MatFormFieldModule,
-    FormsModule, 
+    FormsModule,
     ReactiveFormsModule,
     MatInputModule,
     FontAwesomeModule,
@@ -69,11 +70,12 @@ import { AccessDeniedComponent } from './components/access-denied/access-denied.
     { provide: MatDialogRef, useValue: {} }
   ],
 })
-export class HomeRnfModule { 
+export class HomeRnfModule {
   constructor(library: FaIconLibrary) {
     library.addIconPacks(
       fab,
-      fas
+      fas,
+      far
     );
   }
 }
